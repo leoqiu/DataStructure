2ca4e0c6c3c8a2318c68d6c5678c2433a772ba7d
leoqiu <qiu.shi.727@gmail.com>
2013-08-14 23:14:18 -0700

    mac 7/24

--- DataStructure/src/DP/problems.java
+++ DataStructure/src/DP/problems.java
@@ -32,8 +32,11 @@ public class problems {
 
         if(n==0 || n==1)
             return 2;
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++) {
             sum += 2 * tR(i) * tR(i-1);
+            //System.out.println(sum);
+        }
+
 
         return sum;
     }
@@ -44,27 +47,29 @@ public class problems {
         int t1 = 2;
         int t2 = 2 * t0 * t1;
 
-        //extra table for memorization
+        //extra table for memorization to record every sub-sum
         ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(t0);
-        list.add(t1);
-        list.add(t2);
+        list.add(t0);         //2
+        list.add(t1);         //2
+        list.add(t2);         //8
 
         int ti = 0;
 
+        //from index 3 to calculate cuz already add 3 elements
         for (int i = 3; i <= n; i++) {
 
-            ti = 2 * list.get(i-1) * list.get(i-2);
+            //ti = 2 * (list.get(i-1) + list.get(i-2) )* list.get(i-2);
+            ti = list.get(i-1) + 2 * list.get(i-1) * list.get(i-2);
             list.add(ti);
 
         }
 
-        int tn = 0;
-        for (int i = 0; i < list.size(); i++) {
-            tn += list.get(i);
-        }
+//        int tn = 0;
+//        for (int i = 2; i < list.size(); i++) {
+//            tn += list.get(i);
+//        }
 
-        return tn;
+        return list.get(list.size() - 1);
 
     }
 
@@ -77,8 +82,10 @@ public class problems {
     public static void main (String[] args) {
 
         problems p = new problems();
-        System.out.println(p.tDP(3));
-        System.out.println(p.tR(3));
+        System.out.println(p.tDP(6));
+        System.out.println(p.tR(6));
+
+        //p.tR(3);
 
     }
 
