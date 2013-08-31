4459fc65c41cd50aeb2d4c37783682d6d7f97a8a
leoqiu <qiu.shi.727@gmail.com>
2013-08-31 11:41:37 -0700

    mac 7/24

--- DataStructure/src/GeeksforGeeksAlgorithms/Strings/problems.java
+++ DataStructure/src/GeeksforGeeksAlgorithms/Strings/problems.java
@@ -198,9 +198,10 @@ public class problems {
         int[][] dpArr = new int[charArr1.length+1][charArr2.length+1];
         //string index record
         char[][] dpArr2 = new char[charArr1.length+1][charArr2.length+1];
-        int lcsLen = 0;
+        int currentLcsLen = 0;
+        int maxLcsLen = 0;
 
-        for (int i = 0; i <= charArr1.length; i++) {
+        for (int i = 0; i < charArr1.length; i++) {
             for (int j = 0; j < charArr2.length; j++) {
 
                 if (i == 0 || j == 0)
@@ -208,7 +209,11 @@ public class problems {
                 else if (charArr1[i-1] == charArr2[j-1]) {
                     dpArr[i][j] = dpArr[i-1][j-1] + 1;
                     dpArr2[i][j] = '\\';
-                    lcsLen = Math.max(lcsLen, dpArr[i][j]);
+                    currentLcsLen = Math.max(currentLcsLen, dpArr[i][j]);
+
+                    if(currentLcsLen > maxLcsLen)
+                        maxLcsLen = currentLcsLen;
+
                 }else
                     dpArr[i][j] = 0;
             }
@@ -222,14 +227,53 @@ public class problems {
             System.out.println();
 
         }
+
+        System.out.println(maxLcsLen);
+
+        //print the LCS
+        printLCS(dpArr, maxLcsLen, charArr2);
+
     }
+
+    private void printLCS (int[][] dpArr, int maxLen, char[] charArr2) {
+
+        int indexOfLast1 = -1;
+        int indexOfLast2 = -1;
+
+        for (int i = 0 ; i < dpArr.length; i++) {
+            for (int j = 0; j < dpArr[0].length; j++) {
+                if (dpArr[i][j] == maxLen) {
+                    indexOfLast1 = i;
+                    indexOfLast2 = j;
+                }
+            }
+        }
+
+        printStr(dpArr, charArr2, indexOfLast1, indexOfLast2);
+
+    }
+
+    private void printStr (int[][] dpArr, char[] charArr1, int index1, int index2) {
+
+        if(dpArr[index1][index2] == 0){
+            return;
+        }
+        else {
+            printStr(dpArr, charArr1, index1 - 1, index2 - 1);
+            //charArr start from index 1 of dpArr
+            System.out.print(charArr1[index1-1] + " ");
+        }
+
+    }
+
+
 
 
     /**
      *
      * Longest common substring among 3 string
      *
-     * http://stackoverflow.com/questions/7950244/longest-common-subsequence-among-3-strings
+     * http://stackoverflow.com/questions/7950244/longest-common-substring-among-3-strings
      *
      * @param
      */
@@ -239,7 +283,121 @@ public class problems {
         String str2 = "NewSite:GeeksQuiz.com";
         String str3 = "HAHANewSite:GeeksQduiz.com";
 
+        char[] charArr1 = str1.toCharArray();
+        char[] charArr2 = str2.toCharArray();
+        char[] charArr3 = str3.toCharArray();
+
+        for (int i = 0; i < charArr1.length; i++) {
+
+            for (int j = 0; j < charArr2.length; j++) {
+
+                for (int m = 0; m < charArr3.length; m++) {
+
+
+
+                }
+
+            }
+
+
+        }
+
     }
+
+    /**
+     * longest common substring dp solution
+     *
+     * dpArr[i][j][k] = dpArr[i-1][j-1][k-1] + 1 , charArr1[i] == charArr2[j] == charArr[k]
+     *
+     */
+    public void lcSubStrDP3StrDP () {
+
+        String str1 = "OldSite:GeeksforGeeks.org";
+        String str2 = "NewSite:GeeksQuiz.com";
+        String str3 = "HAHANewSite:GeeksQduiz.com";
+
+        char[] charArr1 = str1.toCharArray();
+        char[] charArr2 = str2.toCharArray();
+        char[] charArr3 = str3.toCharArray();
+
+        int curLen = 0;
+        int maxLen = 0;
+
+        int[][][] dpArr = new int[charArr1.length+1][charArr2.length+1][charArr3.length+1];
+
+        for (int i = 0; i < dpArr.length; i++) {
+
+            for (int j = 0; j < dpArr[i].length; j++) {
+
+                for (int k = 0; k < dpArr[i][j].length; k++) {
+
+                    if (i == 0 || j == 0 || k == 0) {
+                        dpArr[i][j][k] = 0;
+                    } else if ( (charArr1[i-1] == charArr2[j-1]) && (charArr2[j-1] == charArr3[k-1])) {
+
+                        dpArr[i][j][k] = dpArr[i-1][j-1][k-1] + 1;
+                        curLen = Math.max(curLen, dpArr[i][j][k]);
+
+                        if(curLen > maxLen)
+                            maxLen = curLen;
+
+                    } else
+                        dpArr[i][j][k] = 0;
+                }
+            }
+        }
+
+        //print 3 dimensions array
+
+        //print the longest common substring of 3 strings
+        printLCS3StrDP(dpArr, maxLen, charArr1);
+
+
+    }
+
+    private void printLCS3StrDP (int[][][] dpArr, int maxLen, char[] charArr1) {
+
+        int index1 = -1;
+        int index2 = -1;
+        int index3 = -1;
+
+        for (int i = 0; i < dpArr.length; i++) {
+
+            for (int j = 0; j < dpArr[i].length; j++) {
+
+                for (int k = 0; k < dpArr[i][j].length; k++) {
+
+                    if(dpArr[i][j][k] == maxLen) {
+                        index1 = i;
+                        index2 = j;
+                        index3 = k;
+                    }
+                }
+            }
+        }
+
+        printStr3StrDP (dpArr, charArr1, index1, index2, index3);
+
+    }
+
+    private void printStr3StrDP (int[][][] dpArr, char[] charArr1, int index1, int index2, int index3) {
+
+        if(dpArr[index1][index2][index3] == 0)
+            return;
+        else {
+            printStr3StrDP (dpArr, charArr1, index1 - 1, index2 - 1, index3 - 1);
+            System.out.print(charArr1[index1-1] + " ");
+
+        }
+    }
+
+
+    /**
+     * Common prefix of three string
+     *
+     *
+     * @param
+     */
 
 
     public static void main (String[] args) {
@@ -247,7 +405,8 @@ public class problems {
         //p.removeBAndAC();
         //p.removeBAndACImproved();
         //p.lcSubStr();
-        p.lcSubStrDP();
+        //p.lcSubStrDP();
+        p.lcSubStrDP3StrDP();
     }
 
 
