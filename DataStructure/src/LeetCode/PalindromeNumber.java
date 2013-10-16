ebc83f8d633aaf726f2ec588afb3910852430a92
leoqiu <qiu.shi.727@gmail.com>
2013-10-15 22:32:23 -0700

    mac 7/24

--- DataStructure/src/LeetCode/PalindromeNumber.java
+++ DataStructure/src/LeetCode/PalindromeNumber.java
@@ -12,6 +12,93 @@ package LeetCode;
 public class PalindromeNumber {
 
 
+    public boolean isPalindrome(int x) {
 
+        int n = String.valueOf(x).length();
+        int curZeroNums = n-1;
+
+        if (x < 0)
+            return false;
+
+        if (n%2 == 0) {
+
+            while (curZeroNums >= 1) {
+
+                int divisor = (int)Math.pow(10, curZeroNums);
+                int left = x / divisor;
+                int right = x % 10;
+
+                if(left != right)
+                    return false;
+
+                x = (x - left*divisor - right) / 10;
+                curZeroNums = curZeroNums - 2;
+            }
+
+
+        } else {
+
+            while (curZeroNums >= 2) {
+
+                int divisor = (int)Math.pow(10, curZeroNums);
+                int left = x / divisor;
+                int right = x % 10;
+
+                if(left != right)
+                    return false;
+
+                x = (x - left*divisor - right) / 10;
+                curZeroNums = curZeroNums - 2;
+            }
+
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome2(int x) {
+
+        if (x < 0)
+            return false;
+
+        char[] chars = String.valueOf(x).toCharArray();
+
+        int n = chars.length;
+        int left = 0;
+        int right = n-1;
+
+        while (left < right) {
+
+            if (chars[left] != chars[right])
+                return false;
+
+            left++;
+            right--;
+
+        }
+
+
+        return true;
+    }
+
+
+    public static void main (String[] args) {
+        PalindromeNumber s = new PalindromeNumber();
+        System.out.println(s.isPalindrome2(1290550921));
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
